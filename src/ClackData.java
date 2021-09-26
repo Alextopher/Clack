fa48c8fe637e089d5bc570068dd343ea0d5cf72d
humairarezaie <rezaieh@clarkson.edu>
2021-09-26 17:55:55 -0400

    Add ClackData without abstract

---
+++ src/ClackData.java
@@ -0,0 +1,89 @@
+import java.util.Date;
+
+public class ClackData {
+    /**
+     * String representing name of client user
+     */
+    private String userName;
+    /**
+     * Integer representing the type of data stored. One of CONSTANT_LISTUSERS, CONSTANT_LOGOUT,
+     * CONSTANT_SENDMESSAGE, CONSTANT_SENDFILE
+     */
+    private int type;
+    /**
+     * java.util.Date object representing the date when object was created
+     */
+    private Date date;
+
+    /**
+     * Type that represents unknown data type
+      */
+    public static final int CONSTANT_UNKNOWN = -1;
+    /**
+     * Type that represents a listing of all users connected to this session
+      */
+    public static final int CONSTANT_LISTUSERS = 0;
+    /**
+     * Type that requests closure of this client’s connection
+     */
+    public static final int CONSTANT_LOGOUT = 1;
+    /**
+     * Type that signals client wants to send a message
+     */
+    public static final int CONSTANT_SENDMESSAGE = 2;
+    /**
+     * Type that signals client wants to send a file
+     */
+    public static final int CONSTANT_SENDFILE = 3;
+
+    /**
+     * Constructor to set up userName and type. Sets date to current time
+     * @param userName String representing name of client user
+     * @param type Integer representing the type of data stored. One of CONSTANT_LISTUSERS, CONSTANT_LOGOUT,
+     * CONSTANT_SENDMESSAGE, CONSTANT_SENDFILE
+     */
+    public ClackData(String userName, int type) {
+        this.userName = userName;
+        this.type = type;
+        this.date = new Date();
+    }
+
+    /**
+     * Constructor to set type. userName is set to "Anon". Sets date to current time
+     * @param type CONSTANT_LISTUSERS, CONSTANT_LOGOUT, CONSTANT_SENDMESSAGE, CONSTANT_SENDFILE
+     */
+    public ClackData(int type) {
+        new ClackData("Anon", type);
+    }
+
+    /**
+     * Default constructor. userName is set to "Anon". date is set to current time. type is set to CONSTANT_UNKNOWN
+     */
+    public ClackData() {
+        new ClackData(CONSTANT_UNKNOWN);
+    }
+
+    /**
+     * Returns the userName of the client
+     * @return username
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * Returns the type of data
+     * @return type
+     */
+    public int getType() {
+        return type;
+    }
+
+    /**
+     * Returns the time the data was created
+     * @return date
+     */
+    public Date getDate() {
+        return date;
+    }
+}
